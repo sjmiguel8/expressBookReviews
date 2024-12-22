f0ec871e8ca5e6ec3cab4f1988c6aa701d0bbf2a
SJMiguel8 <bonilla.miguel.e@gmail.com>
2024-12-22 18:29:59 -0500

    task12 and 13

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,14 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {}, "url": "https://en.wikipedia.org/wiki/Things_Fall_Apart" },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {}, "url": "https://www.amazon.com/Fairy-Deluxe-Hardbound-Christian-Andersen/dp/935440670X" },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {}, "url": "https://en.wikipedia.org/wiki/Divine_Comedy" },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {}, "url": "https://en.wikipedia.org/wiki/Epic_of_Gilgamesh" },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {}, "url": "https://en.wikipedia.org/wiki/Book_of_Job" },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {}, "url": "https://en.wikipedia.org/wiki/One_Thousand_and_One_Nights" },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {}, "url": "https://en.wikipedia.org/wiki/Nj%C3%A1ls_saga" },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {}, "url": "https://en.wikipedia.org/wiki/Pride_and_Prejudice" },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {}, "url": "https://en.wikipedia.org/wiki/P%C3%A8re_Goriot" },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {}, "url": "https://www.goodreads.com/book/show/12279.Molloy_Malone_Dies_The_Unnamable" },
+      "9780385474542": {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {}, "url": "https://en.wikipedia.org/wiki/Things_Fall_Apart" },
+      "9789354406706": {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {}, "url": "https://www.amazon.com/Fairy-Deluxe-Hardbound-Christian-Andersen/dp/935440670X" },
+      "9789358565140": {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {}, "url": "https://en.wikipedia.org/wiki/Divine_Comedy" },
+      "9780140441000": {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {}, "url": "https://en.wikipedia.org/wiki/Epic_of_Gilgamesh" },
+      "9798839886704": {"author": "Unknown","title": "The Book Of Job", "reviews": {}, "url": "https://en.wikipedia.org/wiki/Book_of_Job" },
+      "9780140442892": {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {}, "url": "https://en.wikipedia.org/wiki/One_Thousand_and_One_Nights" },
+      "9780140447699": {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {}, "url": "https://en.wikipedia.org/wiki/Nj%C3%A1ls_saga" },
+      "9789387779679": {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {}, "url": "https://en.wikipedia.org/wiki/Pride_and_Prejudice" },
+      "9780199538751": {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {}, "url": "https://en.wikipedia.org/wiki/P%C3%A8re_Goriot" },
+      "9780375400704": {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {}, "url": "https://www.goodreads.com/book/show/12279.Molloy_Malone_Dies_The_Unnamable" },
 }
 
 module.exports=books;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,32 +5,17 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 const axios = require('axios');
 
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  const username = req.body.username;
-  const password = req.body.password;
-   // Check if both username and password are provided
-    if (username && password) {
-        // Check if the user does not already exist
-        if (!doesExist(username)) {
-            // Add the new user to the users array
-            users.push({"username": username, "password": password});
-            return res.status(200).json({message: "User successfully registered. Now you can login"});
-        } else {
-            return res.status(404).json({message: "User already exists!"});
-        }
-    }
-    // Return error if username or password is missing
-    return res.status(404).json({message: "Unable to register user."});
+// Create a map of books by author
+let booksByAuthorMap = new Map();
+Object.values(books).forEach(book => {
+  const author = book.author.toLowerCase();
+  if (!booksByAuthorMap.has(author)) {
+    booksByAuthorMap.set(author, []);
+  }
+  booksByAuthorMap.get(author).push(book);
 });
 
-// Get the book list available in the shop
-public_users.get('/', function (req, res) {
-    res.status(200).json(Object.values(books));
-  });
-// Replace with the actual URLs of your books APIs
 const bookUrls = Object.values(books).map(book => book.url);
-
 
 const fetchBooks = () => {
     return new Promise((resolve, reject) => {
@@ -45,9 +30,23 @@ const fetchBooks = () => {
         });
     });
   };
-  
-  // Get the book list available in the shop using Promises
-  public_users.get('/', function (req, res) {
+
+public_users.post("/register", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    if (username && password) {
+      if (!doesExist(username)) {
+        users.push({ "username": username, "password": password });
+        return res.status(200).json({ message: "User successfully registered. Now you can login" });
+      } else {
+        return res.status(404).json({ message: "User already exists!" });
+      }
+    }
+    return res.status(404).json({ message: "Unable to register user." });
+  });
+
+// Get the book list available in the shop
+public_users.get('/', function (req, res) {
     fetchBooks()
       .then((books) => {
         res.status(200).json(books);
@@ -55,43 +54,62 @@ const fetchBooks = () => {
       .catch((error) => {
         res.status(500).json({ message: error.message });
       });
-});
+  });
 
-// Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  const isbn = req.params.isbn;
-  const book = books[isbn];
+// Get book details based on ISBN using async-await with Axios
+public_users.get('/isbn/:isbn', async function (req, res) {
+    const isbn = req.params.isbn;
+    const book = books[isbn];
     if (book) {
-        res.status(200).json(book);
-    } else {
-        res.status(404).json({message: "No books found by ISBN"});
-    }
-} );
-  
-// Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  const author = req.params.author;
-  const booksByAuthor = Object.values(books).filter(book => book.author === author);
-    if (booksByAuthor.length > 0) {
-        res.status(200).json(booksByAuthor);
-    } else {
-        res.status(404).json({message: "No books found by author"});
-    }
-});
-
-// Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-    //Write your code here
-    const title = req.params.title;
-    const booksByTitle = Object.values(books).filter(book => book.title.toLowerCase() === title.toLowerCase());
-     
-    if (booksByTitle.length > 0) {
-          res.status(200).json(booksByTitle);
-      } else {
-          res.status(404).json({message: "No books found by this title"});
+      try {
+        const response = await axios.get(book.url);
+        res.status(200).json(response.data);
+      } catch (error) {
+        res.status(500).json({ message: error.message });
       }
+    } else {
+      res.status(404).json({ message: "Book not found" });
+    }
+  });
+  
+// Get book details based on author 
+public_users.get('/author/:author', async function (req, res) {
+    const author = req.params.author.toLowerCase();
+    const booksByAuthor = booksByAuthorMap.get(author) || [];
+    if (booksByAuthor.length > 0) {
+      try {
+        const bookDetails = booksByAuthor.map(book => ({
+          title: book.title,
+          author: book.author,
+          url: book.url
+        }));
+        res.status(200).json(bookDetails);
+      } catch (error) {
+        res.status(500).json({ message: error.message });
+      }
+    } else {
+      res.status(404).json({ message: "No books found by author" });
+    }
+  });
+  
+  // Get all books based on title
+  public_users.get('/title/:title', async function (req, res) {
+    const title = req.params.title.toLowerCase();
+    const booksByTitle = Object.values(books).filter(book => book.title.toLowerCase() === title);
+    if (booksByTitle.length > 0) {
+      try {
+        const bookDetails = booksByTitle.map(book => ({
+          title: book.title,
+          author: book.author,
+          url: book.url
+        }));
+        res.status(200).json(bookDetails);
+      } catch (error) {
+        res.status(500).json({ message: error.message });
+      }
+    } else {
+      res.status(404).json({ message: "No books found by this title" });
+    }
   });
 
 //  Get book review
