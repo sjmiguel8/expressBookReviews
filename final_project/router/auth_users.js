2b6256a33e1de5794cd4dcbc3de559cfdd262e61
SJMiguel8 <bonilla.miguel.e@gmail.com>
2024-12-21 23:53:39 -0500

    task 7 complete

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -3,20 +3,78 @@ const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
-let users = [];
+let users = [  
+  { username: 'user1', password: 'password1' },
+  { username: 'user2', password: 'password2' }
+];
 
-const isValid = (username)=>{ //returns boolean
+function isValid(username) {
+  return users.some(user => user.username === username);
+//returns boolean
 //write code to check is the username is valid
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+function doesExist(username) {
+  return users.some(user => user.username === username);
 }
+
+const authenticatedUser = (username, password) => {
+    // Filter the users array for any user with the same username and password
+    let validusers = users.filter((user) => {
+        return (user.username === username && user.password === password);
+    });
+    // Return true if any valid user is found, otherwise false
+    if (validusers.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+//returns boolean
+//write code to check if username and password match the one we have in records.
+regd_users.post("/register", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username && password) {
+    if (!doesExist(username)) {
+      users.push({ "username": username, "password": password });
+      return res.status(200).json({ message: "User successfully registered. Now you can login" });
+    } else {
+      return res.status(404).json({ message: "User already exists!" });
+    }
+  }
+  return res.status(404).json({ message: "Unable to register user." });
+});
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+const username = req.body.username;
+const password = req.body.password;
+
+    // Check if username or password is missing
+    if (!username || !password) {
+        return res.status(404).json({ message: "Error logging in" });
+    }
+
+    if (!isValid(username)) {
+        return res.status(404).json({ message: "Invalid username" });
+    }
+    // Authenticate user
+    if (authenticatedUser(username, password)) {
+        // Generate JWT access token
+        let accessToken = jwt.sign({
+            data: password
+        }, 'access', { expiresIn: 60 * 60 });
+
+        // Store access token and username in session
+        req.session.authorization = {
+            accessToken, username
+        }
+        return res.status(200).send("User successfully logged in");
+    } else {
+        return res.status(208).json({ message: "Invalid Login. Check username and password" });
+    }
 });
 
 // Add a book review
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -57,14 +57,28 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+    //Write your code here
+    const title = req.params.title;
+    const booksByTitle = Object.values(books).filter(book => book.title.toLowerCase() === title.toLowerCase());
+     
+    if (booksByTitle.length > 0) {
+          res.status(200).json(booksByTitle);
+      } else {
+          res.status(404).json({message: "No books found by this title"});
+      }
+  });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+    const isbn = req.params.isbn;
+    const book = books[isbn];
+    
+    if (book && book.reviews) {
+        res.status(200).json(book.reviews);
+    } else {
+        res.status(404).json({message: "No reviews found for this ISBN"});
+    }
+    //Write your code here
+  });
 
 module.exports.general = public_users;
